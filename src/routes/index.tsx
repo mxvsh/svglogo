5ad397f2215997593accb34098b12997d84fcd1c
mxvsh <hello@monawwar.io>
2026-03-12 03:25:24 +0530

    Animate feedback button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -108,7 +108,21 @@ function Editor() {
 			</motion.div>
 			<Dock />
 			<IconPickerModal />
-			<div className="pointer-events-auto absolute bottom-4 right-4 z-20 flex flex-col items-end gap-2">
+			<motion.div
+				initial={{ opacity: 0, y: 20, scale: 0.96 }}
+				animate={{ opacity: 1, y: [0, -2, 0], scale: 1 }}
+				transition={{
+					opacity: { duration: 0.35, delay: 0.25, ease: [0.22, 1, 0.36, 1] },
+					scale: { duration: 0.35, delay: 0.25, ease: [0.22, 1, 0.36, 1] },
+					y: {
+						duration: 3.6,
+						delay: 0.6,
+						repeat: Number.POSITIVE_INFINITY,
+						ease: "easeInOut",
+					},
+				}}
+				className="pointer-events-auto absolute bottom-4 right-4 z-20 flex flex-col items-end gap-2"
+			>
 				<a
 					href={FEEDBACK_URL}
 					target="_blank"
@@ -127,7 +141,7 @@ function Editor() {
 					<Icon icon="simple-icons:discord" width={12} height={12} />
 					Join Discord
 				</a>
-			</div>
+			</motion.div>
 		</div>
 	);
 }
