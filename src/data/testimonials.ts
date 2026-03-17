4a75f3ed143f62cc9fd23b1c81f019a8a1a69518
mxvsh <hello@monawwar.io>
2026-03-18 01:27:11 +0530

    Update testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -11,7 +11,7 @@ export const TESTIMONIALS: Testimonial[] = [
       "I can't tell you how many times I've spent way too much time in Figma just to make a simple placeholder logo.",
     author: "u/char0dey",
     source: "reddit",
-    url: "https://reddit.com/user/char0dey",
+    url: "https://www.reddit.com/r/SideProject/comments/1rv9hot/comment/oarkmy6",
   },
   {
     quote: "This is great! I'm working on a few MVPs and I'll likely use it.",
@@ -23,18 +23,18 @@ export const TESTIMONIALS: Testimonial[] = [
     quote: "Great job! I like it. Much better than messing with Canva.",
     author: "u/Developer_Memento",
     source: "reddit",
-    url: "https://reddit.com/user/Developer_Memento",
+    url: "https://www.reddit.com/r/SideProject/comments/1rv9hot/comment/oarwc96",
   },
   {
     quote: "Love this. I will give it a try! I am a designer and love this ha!",
     author: "u/hparamore",
     source: "reddit",
-    url: "https://reddit.com/user/hparamore",
+    url: "https://www.reddit.com/r/SideProject/comments/1rv9hot/comment/oas547t",
   },
   {
     quote: "Super polished and great for this use case. Nice work!",
     author: "u/webmonarch",
     source: "reddit",
-    url: "https://reddit.com/user/webmonarch",
+    url: "https://www.reddit.com/r/SideProject/comments/1rv9hot/comment/oasc66n",
   },
 ];
