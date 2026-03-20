904d885c981500b4395176de0146dfd7028f55f5
mxvsh <hello@monawwar.io>
2026-03-20 23:20:20 +0530

    Simplify loadLogoFromState with Object.assign

--- src/commands/logo/load-logo.ts
+++ src/commands/logo/load-logo.ts
@@ -11,15 +11,6 @@ export function loadLogo(raw: unknown) {
 
 export function loadLogoFromState(logo: LogoState) {
   useLogoStore.getState().set((d) => {
-    d.iconName = logo.iconName;
-    d.iconColor = logo.iconColor;
-    d.iconBorderColor = logo.iconBorderColor;
-    d.iconBorderWidth = logo.iconBorderWidth;
-    d.iconSize = logo.iconSize;
-    d.iconRotation = logo.iconRotation;
-    d.background = logo.background;
-    d.borderRadius = logo.borderRadius;
-    d.borderWidth = logo.borderWidth;
-    d.borderColor = logo.borderColor;
+    Object.assign(d, logo);
   });
 }
